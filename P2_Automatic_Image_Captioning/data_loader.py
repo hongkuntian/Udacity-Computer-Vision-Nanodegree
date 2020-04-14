8f61d9e072ff1789921d5e8c1082b3cdfbc1ce83
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-04-13 21:07:30 -0400

    Add updated Udacity workspace files

--- P2_Automatic_Image_Captioning/data_loader.py
+++ P2_Automatic_Image_Captioning/data_loader.py
@@ -1,3 +1,4 @@
+
 import nltk
 import os
 import torch
--- P2_Automatic_Image_Captioning/model.py
+++ P2_Automatic_Image_Captioning/model.py
@@ -23,11 +23,52 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        # Word embedding layer
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        
+        # LSTM cell
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        
+        # Fully connected output layer
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        
     
     def forward(self, features, captions):
-        pass
+        # Embed the captions
+        embeded_captions = self.embedding(captions[:, :-1])
+        
+        # Concatenate feature and embeded captions into single tensor
+        inputs = torch.cat([features.unsqueeze(1), embeded_captions], 1)
+        
+        # Pass inputs to LSTM
+        output, _ = self.lstm(inputs)
+        
+        output = self.fc(output)
+        
+        return output
+        
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        
+        sentence = []
+        
+        for i in range(max_len):
+            output, states = self.lstm(inputs, states)
+            output = self.fc(output)
+            output = output.squeeze(1)
+            _, predicted_index = torch.max(output, dim=1)
+            word_ind = int(predicted_index.cpu().numpy()[0])
+            sentence.append(word_ind)
+            
+            # Break from loop once we reach <end> token
+            if predicted_index == 1:
+                break
+                
+            inputs = self.embedding(predicted_index)
+            inputs = inputs.unsqueeze(1)
+        
+        return sentence
+        
